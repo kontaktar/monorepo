6ea9220f070a2bdff7ce6a4dd2c65a6b5e27d778
einaralex <einaralex@gmail.com>
2026-02-12 01:27:05 +0000

    build fix

--- packages/database/tsup.config.ts
+++ packages/database/tsup.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/server.ts"],
+  entry: ["src/index.ts"],
   format: ["esm"],
-  dts: false,
+  dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
   outDir: "dist",
   target: "node18",
-  external: ["@kontaktar/database", "@repo/logger"],
 });
